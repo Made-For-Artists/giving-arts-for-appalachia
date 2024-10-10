aea68bdb2a2ca44c0f62c1749f6935fdbf78561d
Kevin I Rivera <kevin.ivan05@gmail.com>
2024-10-10 13:22:10 -0700

    add option to mark artwork as purchased

--- src/features/artwork/ArtworkPrice.tsx
+++ src/features/artwork/ArtworkPrice.tsx
@@ -4,12 +4,18 @@ import type { Artwork } from '~/lib/client.types.ts';
 type Props = {
 	price?: Artwork['formattedPrice'];
 	class?: string;
+    purchased?: boolean;
 };
 
 export function ArtworkPrice(props: Props) {
 	return (
-		<span class={twMerge('flex gap-2 font-medium', props.class)}>
-			{props.price}
-		</span>
+        <>
+            <div class="flex gap-2">
+                <span class={twMerge('flex gap-2 font-medium', props.class, (props.purchased && 'line-through'))}>
+                    {props.price}
+                </span>
+                <span>{props.purchased ? 'GIFTED' : ''}</span>
+            </div>
+        </>
 	);
 }
