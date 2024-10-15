1958de11c608a56bea0c2a8db426092cbf357399
Jacquelyne Bedoya <jacquelyne.bedoya@uabc.edu.mx>
2024-10-14 20:32:02 -0700

    Added faqs to faq page

--- src/components/ui/CollapsibleFaq/CollapsibleFaq.tsx
+++ src/components/ui/CollapsibleFaq/CollapsibleFaq.tsx
@@ -1,19 +1,17 @@
 import { Collapsible } from "@kobalte/core/collapsible";
-import { RiArrowsArrowDownFill } from "solid-icons/ri";
+import { FaSolidPlus } from 'solid-icons/fa'
 import "./CollapsibleFaq.css";
 
-export function CollapsibleFaq() {
+export function CollapsibleFaq({ content, question }) {
   return (
     <Collapsible class="collapsible">
         <Collapsible.Trigger class="collapsible__trigger">
-            <span>What is Kobalte?</span>
-            <RiArrowsArrowDownFill class="collapsible__trigger-icon" />
+            <span>{question}</span>
+            <FaSolidPlus class="collapsible__trigger-icon" />
         </Collapsible.Trigger>
         <Collapsible.Content class="collapsible__content">
             <p class="collapsible__content-text">
-            Kobalte is a UI toolkit for building accessible web apps and design systems with SolidJS.
-            It provides a set of low-level UI components and primitives which can be the foundation
-            for your design system implementation.
+            {content}
             </p>
         </Collapsible.Content>
     </Collapsible>
