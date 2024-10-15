49bd987c302b0e7d425467d50d35d6dbfa7cd8ad
Kevin I Rivera <kevin.ivan05@gmail.com>
2024-10-15 12:07:05 -0700

    remove unused product path

--- src/paths.ts
+++ src/paths.ts
@@ -1,4 +1,3 @@
 import type { Artwork } from "./lib/client.types.ts";
 
 export const artworkPath = (slug: Artwork['slug']) => `/artworks/${slug}`;
-export const productPath = (slug: Artwork['slug']) => `/products/${slug}`;
