4618b25da572be44d330702c98069b0ae34a185f
Kevin I Rivera <kevin.ivan05@gmail.com>
2024-10-18 20:01:16 -0700

    prop drilling

--- src/components/ui/MessageDialog/MessageDialogButton.tsx
+++ src/components/ui/MessageDialog/MessageDialogButton.tsx
@@ -1,15 +1,18 @@
 
 import { MessageDialogStore } from '~/features/cart/store.ts';
+import type { Artwork } from '~/lib/client.types.ts';
 import { Button } from '../Button.tsx';
 import { MessageDialog } from './MessageDialog.tsx';
 
 export interface Props {
     artworkID?: string;
+    artwork?: Artwork;
 }
 
 export function MessageDialogButton(props: Props) {
 	return (
 		<MessageDialog
+            artwork={props.artwork}
             artworkID={props.artworkID}
 			open={MessageDialogStore.messageDialogOpen}
 			onOpenChange={MessageDialogStore.setMessageDialogOpen}
