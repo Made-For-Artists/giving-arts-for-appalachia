784493b718424b79d9a040f280febff1479db5d0
Kevin I Rivera <kevin.ivan05@gmail.com>
2024-10-18 20:54:07 -0700

    implemented sort by price and title; expects full collection to be loaded in memory - must change if native firestore sort is used;

--- src/components/ArtworkCollection/ArtworkCollection.tsx
+++ src/components/ArtworkCollection/ArtworkCollection.tsx
@@ -5,19 +5,26 @@ import ArtworkCard from '~/features/artwork/ArtworkCard.tsx';
 import { attachFirestoreCollectionListener } from '~/firebase/client.ts';
 import type { Artwork } from '~/lib/client.types.ts';
 
-export default function ArtworkCollection({ collection }) {
+interface Props {
+    collection: string;
+    sort?: string;
+    order?: string;
+}
+
+export default function ArtworkCollection({ collection, sort, order } : Props) {
 	const [artworks, setArtworks] = createSignal<Artwork[]>([]);
 
     if (collection === 'all') {
         attachFirestoreCollectionListener('artworks', (snapshot: QuerySnapshot) => {
+            // Firestore response
             const arr : Artwork[] = [];
             snapshot.forEach((doc) => {
                 let curData = doc.data()
                 curData.id = doc.id
                 arr.push(curData as Artwork);
             });
-            console.log('artworks:', arr)
-            setArtworks(arr)
+
+            setArtworks(organizeData(sort, order, arr))
         });
     } else {
         attachFirestoreCollectionListener('artworks', (snapshot: QuerySnapshot) => {
@@ -25,10 +32,26 @@ export default function ArtworkCollection({ collection }) {
             snapshot.forEach((doc) => {
                 arr.push(doc.data() as Artwork);
             });
-            console.log('artworks:', arr)
-            setArtworks(arr)
+
+            setArtworks(organizeData(sort, order, arr))
         }, collection);
     }
+
+    const organizeData = (sortProp?: string, orderProp?: string, arr?: Artwork[]) => {
+        if (!arr) return []
+        
+        if (sortProp === 'price') {
+			return arr.sort((a, b) => {
+				return orderProp === 'asc' ? a.price - b.price : b.price - a.price;
+			});
+		} else if (sortProp === 'title') {
+			return arr.sort((a, b) => {
+				return orderProp === 'asc' ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title);
+			});
+		} else {
+            return arr
+        }
+	} 
 
 	return (
 		<>
